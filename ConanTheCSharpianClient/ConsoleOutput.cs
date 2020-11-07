5214c00639e3234b8b40f151a40a3b5227e7e72c
Daniele Banovaz <daniele.banovaz@gmail.com>
2020-11-07 20:18:19 +0100

    Introduced alternate coloring for the console messages

--- ConanTheCSharpianClient/ConsoleOutput.cs
+++ ConanTheCSharpianClient/ConsoleOutput.cs
@@ -7,11 +7,17 @@ namespace ConanTheCSharpian.Client
 {
     public class ConsoleOutput : IMessageHandler
     {
+        private bool _alternateColor;
+
         public void DisplayMessage(string message, bool pause)
         {
+            Console.ForegroundColor = _alternateColor ? ConsoleColor.DarkGray : ConsoleColor.Gray;
+
             Console.WriteLine(message);
             if (pause)
                 Console.ReadKey(true);
+
+            _alternateColor = !_alternateColor;
         }
     }
 }
